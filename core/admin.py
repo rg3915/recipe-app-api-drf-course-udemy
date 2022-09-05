0fb548ff73de2d31e0ed83df981ef89c7f8ebed8
rg3915 <regis42santos@gmail.com>
2022-09-04 22:10:30 -0300

    Add

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import Recipe, User
+from core.models import Recipe, Tag, User
 
 
 class UserAdmin(BaseUserAdmin):
@@ -47,5 +47,5 @@ class RecipeAdmin(admin.ModelAdmin):
 
 
 admin.site.register(User, UserAdmin)
-# admin.site.register(Tag)
+admin.site.register(Tag)
 # admin.site.register(Ingredient)
