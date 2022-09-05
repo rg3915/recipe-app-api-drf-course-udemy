6665a250070a91e5d2c2fe9db6fc2db44a728102
rg3915 <regis42santos@gmail.com>
2022-09-05 02:02:19 -0300

    Fix MEDIA_ROOT

--- app/settings.py
+++ app/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 
 # Default primary key field type
